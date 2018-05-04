2149ed82fda62058549c2c195ef67db05fd82fdb
bmitchinson <33632286+bmitchinson@users.noreply.github.com>
2018-05-04 16:53:33 -0500

    Tests finished

--- test/NormEuclideanDistTest.java
+++ test/NormEuclideanDistTest.java
@@ -1,7 +1,6 @@
-import org.junit.Test;
 import edu.uiowa.cs.similarity.DB.WordDB;
 import edu.uiowa.cs.similarity.SimilarityFunctions.NegEuclideanDist;
-import edu.uiowa.cs.similarity.Vectors.GenericVector;
+import edu.uiowa.cs.similarity.SimilarityFunctions.NormEuclideanDist;
 import edu.uiowa.cs.similarity.Vectors.SemanticVector;
 import org.junit.Test;
 
@@ -9,12 +8,12 @@ import java.util.ArrayList;
 import java.util.Map;
 
 import static junit.framework.TestCase.assertEquals;
-import static org.junit.Assert.assertTrue;
-public class NegEuclideanDistTest {
+
+
+public class NormEuclideanDistTest {
 
     @Test
-    public void negEuclideanEasy() {
-        //Calling top j for top 2 on "project" vectorArray[3].getWord() with negEuc function
+    public void normTestEasy() {
         WordDB testDB = new WordDB();
         testDB.index("src/data/junit_Easy.txt");
 
@@ -26,16 +25,15 @@ public class NegEuclideanDistTest {
             i++;
         }
 
-        ArrayList<Map.Entry<String,Double>> result = testDB.TopJ(vectorArray[3].getWord(), 8, new NegEuclideanDist());
+        ArrayList<Map.Entry<String,Double>> result = testDB.TopJ(vectorArray[3].getWord(), 8, new NormEuclideanDist());
         //System.out.println(result);
-        assertEquals(result.get(0).getValue(), -3.16227, .00001);
-        assertEquals(result.get(7).getValue(), -3.87298, .00001);
-        assertEquals(result.get(1).getValue(), -3.16227, .00001);
-        assertEquals(result.get(2).getValue(), -3.16227, .00001);
+        assertEquals(result.get(0).getValue(), -1.16774, .00001);
+        assertEquals(result.get(1).getValue(), -1.20482, .00001);
+        assertEquals(result.get(5).getValue(), -1.24255, .00001);
     }
 
     @Test
-    public void negEuclideanMedium() {
+    public void normTestMedium() {
         //Calling top j for top 2 on "project" vectorArray[3].getWord() with negEuc function
         WordDB testDB = new WordDB();
         testDB.index("src/data/vector_test.txt");
@@ -51,13 +49,12 @@ public class NegEuclideanDistTest {
         ArrayList<Map.Entry<String,Double>> result = testDB.TopJ(vectorArray[12].getWord(), 8, new NegEuclideanDist());
         //System.out.println(result);
         assertEquals(result.get(0).getValue(), -4.12310, .00001);
-        assertEquals(result.get(3).getValue(), -4.35889, .00001);
+        assertEquals(result.get(2).getValue(), -4.35889, .00001);
         assertEquals(result.get(4).getValue(), -4.69041, .00001);
-        assertEquals(result.get(5).getValue(), -5.91607, .00001);
     }
 
     @Test
-    public void negEuclideanHard() {
+    public void normTestHard() {
         //Calling top j for top 2 on "project" vectorArray[3].getWord() with negEuc function
         WordDB testDB = new WordDB();
         testDB.index("src/data/junit_Hard.txt");
@@ -66,15 +63,15 @@ public class NegEuclideanDistTest {
         int i = 0;
         for (SemanticVector vector : testDB.getVectors()) {
             vectorArray[i] = vector;
-            if(i<10){vector.print();}
+            //if(i<10){vector.print();}
             i++;
         }
 
         ArrayList<Map.Entry<String,Double>> result = testDB.TopJ(vectorArray[3].getWord(), 8, new NegEuclideanDist());
-        System.out.println(result);
-        assertEquals(result.get(0).getValue(), -18.33030, .00001);
-        assertEquals(result.get(0).getValue(), -18.33030, .00001);
-        assertEquals(result.get(0).getValue(), -18.33030, .00001);
-        assertEquals(result.get(0).getValue(), -18.33030, .00001);
+        //System.out.println(result);
+        assertEquals(result.get(0).getValue(), -21.86321, .00001);
+        assertEquals(result.get(1).getValue(), -21.86321, .00001);
+        assertEquals(result.get(2).getValue(), -21.86321, .00001);
+        assertEquals(result.get(3).getValue(), -21.86321, .00001);
     }
 }
